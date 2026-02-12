2bab41d429c590e11aeed53345e44969a3004ff1
jliu879 <103475434+jliu879@users.noreply.github.com>
2026-02-12 17:29:09 +0000

    Deploying to gh-pages from @ jliu879/jliu879.github.io@626948e635782ce7203eac51401fac42716887e7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,7 +518,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6A%69%65%74%69%61%6E@%75%6D%69%63%68.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -539,14 +539,21 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=iG1ucuEAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.linkedin.com/in/jietian-liu-65612b214", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/jliu879", "_blank");
         },
       },{
       id: 'light-theme',
